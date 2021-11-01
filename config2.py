97a776f578fa29f34733f94aee7d20941261e76b
akansha-nec <91887826+akansha-nec@users.noreply.github.com>
2021-11-01 10:03:07 +0530

    Add files via upload

---
+++ config2.py
@@ -0,0 +1,33 @@
+    client id =  A3T6AKIAFJKR45SAWS5Z
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+ 
+    
+    client id =  A3T6AKIAFJKR1245SAWS
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+    client id =  DUMMY_A3T6AKIAFJKDUMMYSAWS
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+
+https://ap-south-1.console.aws.amazon.com/console/home?region=ap-south-1#
+
+client id = 932934478677
+
+Canonical User ID: df024bd34fe06869b1d7f887da00bd20ca3b705c7867efe0f2c0f6d644c8d9eb
+
+Full Name:
+Akansha
+Address:
+90 dikuhsa New katra  
+City:
+allahabad
+State:
+UP
+Postal Code:
+211002
+Country:
+IN
+Phone Number:
++9199976555
+Company Name:
+Website URL:
